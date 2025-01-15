970d3dc0ac329710a305bc467494336fa9bdce1e
tjay <jaikumartjay@gmail.com>
2025-01-15 17:21:16 +0530

    v-2.3 -- SECURITY UPDATE

--- src/fireConfig.js
+++ src/fireConfig.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDaKXELbEW3jCcTGvQ3R58yfxGXRa2-rm0",
-  authDomain: "kisstutorial-abf6d.firebaseapp.com",
-  projectId: "kisstutorial-abf6d",
-  storageBucket: "kisstutorial-abf6d.firebasestorage.app",
-  messagingSenderId: "928621420037",
-  appId: "1:928621420037:web:163cb04aa3f1cbd2d0866c"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId:process.env.REACT_APP_FIREBASE_APP_ID 
 };
 
 // Initialize Firebase
